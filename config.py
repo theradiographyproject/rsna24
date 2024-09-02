b8618e0d2acd56355d768ec7e302540db2dfc857
Vineeth Gangaram <vineeth.gangaram@gmail.com>
2024-09-02 10:11:57 -0400

    added config file so users can define where to download and process files to

---
+++ config.py
@@ -0,0 +1,10 @@
+# Base path for all raw data
+raw_data_path = ""
+
+# Titles of the datasets when downloaded
+RSNA_2024_competition = 'rsna-2024-lumbar-spine-degenerative-classification'
+LSD_dataset = 'LSD'
+
+# Output directories for the processed datasets
+RSNA_processed = ''
+LSD_processed = ''
